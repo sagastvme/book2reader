06ffa88502a224422e6087b4b3bca511ed2997b0
Eduardo <miltormentas@tutanota.com>
2026-06-02 12:13:52 -0400

    named home route

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\FormController;
 use App\Http\Middleware\DeviceIsReader;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [FormController::class, 'index'])->middleware(DeviceIsReader::class);
+Route::get('/', [FormController::class, 'index'])->middleware(DeviceIsReader::class)->name('home');
